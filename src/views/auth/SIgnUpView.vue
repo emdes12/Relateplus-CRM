51e8bec7149a7662c56486fb73e1d6447125283e
Alawiye <naijabayz@gmail.com>
2025-02-05 10:46:08 +0100

    successfully done the containers responsivesness

--- src/views/auth/SIgnUpView.vue
+++ src/views/auth/SIgnUpView.vue
@@ -1,24 +1,27 @@
 <script setup>
 import Logo from "../../assets/icons/r+.svg"
+import AuthImg from "../../assets/images/auth-sign-up.png"
 </script>
 
 <template>
-  <div class="container auth">
-    <div class="logo"><img alt="logo" src="@/assets/logo2.svg" /></div>
-  <main>
-  <div class="content">
-    <div class="form-container">
-      <h2>We're excited you're here</h2>
-      <h4>Start by creating a free account</h4>
-      <form action="#">
-        <input type="text"><br>
-        <input type="text"><br>
-        <button type="submit">Sign Up</button>
-      </form>
+  <div class="auth">
+    <div class="container">
+      <div class="logo"><img alt="logo" src="@/assets/logo2.svg" /></div>
+    <main class="content">
+      <div class="form-container">
+        <h2>We're excited you're here</h2>
+        <h4>Start by creating a free account</h4>
+        <form action="#">
+          <input type="text"><br>
+          <input type="text"><br>
+          <button type="submit">Sign Up</button>
+        </form>
+      </div>
+      <div class="img">
+        <img class="imag" :src="AuthImg" />
+      </div>
+    </main>
     </div>
-    <div class="img"></div>
-  </div>
-  </main>
   </div>
 </template>
 
@@ -27,14 +30,17 @@ import Logo from "../../assets/icons/r+.svg"
   background-color: var(--auth-bg);
   width: 100%;
   height: 100vh;
-  display: grid;
-  grid-template-columns: 1fr;
-  grid-template-rows: 150px 1fr;
 }
 
 .logo {
+  padding: 20px;
   display: grid;
   place-items: center;
+}
+
+.container {
+  display: grid;
+  grid-template-rows: 100px 1fr;
 }
 
 h2, h4 {
@@ -46,45 +52,47 @@ h4 {
   font-weight: 700;
 }
 
-main, .content {
-  display: grid;
-  place-items: center;
-}
-
-.content {
-  grid-template-rows: 602px;
-  grid-template-columns: 1fr 2000px 1fr;
+main.content {
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
 .img {
-  background-image: url("../../assets/images/auth-sign-up.png");
-  background-position: center;
-  background-size: cover;
-  background-repeat: no-repeat;
-  grid-column: 2 / 3;
-  width: 482px;
-  border-radius: 32px;
-  left: -200px;
-  position: sticky;
+  max-width: 452px;
+  border-top-right-radius: 32px;
+  border-bottom-right-radius: 32px;
   height: 494px;
+  overflow: hidden;
+}
+
+img.imag {
+  width: 100%;
+  height: 100%;
+  object-position: center;
+  object-fit: cover;
 }
 
 .form-container {
   background-color: white;
-  width: 482px;
+  width: 627px;
   gap: 20px;
   border-radius: 32px;
   height: 602px;
   display: flex;
   align-items: center;
-  z-index: 12;
   justify-content: space-between;
   flex-direction: column;
   padding: 42px 50px;
-  grid-column: 1 / 3;
 }
 
-@media (min-width: 1024px) {
-  
+@media (max-width: 700px) {
+  .img {
+    display: none;
+  }
+
+  .form-container {
+    width: 100%;
+  }
 }
 </style>
