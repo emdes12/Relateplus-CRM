094c5c800e5a1c1a9a694c9754afdf527700a2b1
Alawiye Muritala <naijabayz@gmail.com>
2025-05-17 05:30:54 +0100

    switch apimode to host

--- apiMode.js
+++ apiMode.js
@@ -8,16 +8,16 @@ import axios from "axios";
 
 // 1. The Railway backend link to connect
 
-// const apiMode = axios.create({
-//   baseURL: "https://relate-server-production.up.railway.app/",
-//   // withCredentials: true // Optional: for cookies/sessions
-// });
+const apiMode = axios.create({
+  baseURL: "https://relate-server-production.up.railway.app/",
+  // withCredentials: true // Optional: for cookies/sessions
+});
 
 // 2. the Local backend for dev
 
-const apiMode = axios.create({
-  baseURL: "http://localhost:4123", // Change this for production
-  // withCredentials: true // Optional: for cookies/sessions
-});
+// const apiMode = axios.create({
+//   baseURL: "http://localhost:4123", // Change this for production
+//   // withCredentials: true // Optional: for cookies/sessions
+// });
 
 export default apiMode;
